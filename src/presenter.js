322d79244a7c1e9f6279e9be7a4e73e40f269a43
Vivian Revollo <revollovivian@gmail.com>
2024-08-13 12:17:52 -0400

    Codigo con multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,16 @@
 import sumar from "./sumador";
+import mult from "./mult"; 
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#sumar-form"); 
 const div = document.querySelector("#resultado-div");
+
+const firstm = document.querySelector("#primer-numero-mult");
+const secondm = document.querySelector("#segundo-numero-mult");
+const form2 = document.querySelector("#multiplicador-form");
+const div2 = document.querySelector("#resultadomult-div");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +19,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(firstm.value);
+  const secondNumber = Number.parseInt(secondm.value);
+
+  div2.innerHTML = "<p>" + mult(firstNumber, secondNumber) + "</p>";
 });
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,14 @@
 import sumar from "./sumador.js";
+import mult from "./mult.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
+  });
+});
+
+describe("Multiplicar", () => {
+  it("deberia mult dos numeros", () => {
+    expect(mult(3, 2)).toEqual(6);
   });
 });
